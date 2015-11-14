cb3f47c94f180786db173134e38fe0aa60b7f2b6
Mark Robson <roppa_uk@hotmail.com>
2015-11-14 15:16:09 +0000

    Prettified

--- server/api/category/categoryRouter.js
+++ server/api/category/categoryRouter.js
@@ -2,14 +2,12 @@ var express = require("express");
 categoryController = require("./categoryController");
 categoryRouter = express.Router({mergeParams: true});
 
-categoryRouter.get('/', categoryController.categories)
-  .post('/', categoryController.insert)
-  .put('/', categoryController.update);
-
 categoryRouter
+  .get('/', categoryController.categories)
   .get('/:id', categoryController.category)
+  .post('/', categoryController.insert)
+  .put('/', categoryController.update)
   .put('/:id', categoryController.updateById)
   .delete('/:id', categoryController.remove);
-
 
 module.exports = categoryRouter;
